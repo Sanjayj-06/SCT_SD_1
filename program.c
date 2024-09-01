f6fe3ff13cbfdc7297779916ad3b09ec8594c5b3
Sanjayj-06 <sanjayjayakumar91@gmail.com>
2024-09-01 17:11:19 +0530

    function declarations

--- program.c
+++ program.c
@@ -1,2 +1,13 @@
 #include <stdio.h>
 #include <conio.h>
+
+int celsius_to_fahrenheit(int tempc , int result);
+int celsius_to_kelvin(int tempc ,int result);
+int fahrenheit_to_celsius(int tempf ,int reslult);
+int fahrenheit_to_kelvin(int tempf , int result);
+int kelvin_to_celsius(int tempk , int result);
+int kelvin_to_fahrenheit(int tempk , int result);
+
+int main(){
+    
+}
